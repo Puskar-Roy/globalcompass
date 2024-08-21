678567dd6a8589116f80d2722aafb9741aa11823
Puskar-Roy <puskarroy600@gmail.com>
2024-08-21 09:09:28 +0530

    feat : changes

--- components/utils/Header.tsx
+++ components/utils/Header.tsx
@@ -4,7 +4,7 @@ import logo from '@/public/logo.png'
 
 const Header = () => {
   return (
-    <nav id="header" className="fixed w-full z-30 top-0 text-white">
+    <nav id="header" className=" w-full text-white">
     <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
       <div className="pl-4 flex items-center">
         <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
